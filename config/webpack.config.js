f6db1407605553f2c37eac303a42e21802653166
ltbringer <amresh.venugopal@gmail.com>
2023-10-30 16:41:39 +0000

    update: webpack compilation

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,31 +1,48 @@
 const path = require('path')
+const glob = require('glob')
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, '../dist/umd'),
-    filename: 'index.js',
-    library: 'exampleTypescriptPackage',
-    libraryTarget: 'umd',
-    globalObject: 'this',
-  },
-  watch: process.env.NODE_ENV === "dev",
-  module: {
-    rules: [
-      {
-        test: /\.ts(x*)?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            configFile: 'config/tsconfig.umd.json',
-          },
+module.exports = (env) => {
+  return {
+    entry: {
+      ts: glob.sync('./src/**/*.ts'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
         },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.tsx', '.jsx'],
-  },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules|\.d\.ts$/,
+        },
+        {
+          test: /\.d\.ts$/,
+          loader: 'ignore-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    devtool: 'inline-source-map',
+    mode: env === 'production' ? 'production' : 'development',
+    watch: process.env.NODE_ENV === 'dev',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, '../dist'),
+      library: 'ltengine',
+      libraryTarget: 'umd',
+      globalObject: 'this',
+    },
+  }
 }

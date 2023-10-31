59ac8d7c2e777c213664a771a4b32e671e0e0ab5
ltbringer <amresh.venugopal@gmail.com>
2023-10-31 03:11:53 +0000

    WIP: particle effects.

--- src/engine/environment/index.ts
+++ src/engine/environment/index.ts
@@ -1,11 +1,13 @@
 import { matrix } from '../../utils/matrix'
-import { Actor, ActorMove } from '../actor'
+import { Actor, ActorEvent } from '../actor'
 import { RenderEngine } from '../rendering';
-import { Entity, Collision } from '../entity';
+import { Entity, Collision, EntityType } from '../entity';
+import { Particle } from '../particle';
 
 export const WALKABLE = 0;
 
-export class Coordinates {
+
+export class Coordinates implements Coordinates {
     x: number;
     y: number;
 
@@ -13,7 +15,26 @@ export class Coordinates {
         this.x = x
         this.y = y
     }
+
+    add(other: Coordinates): Coordinates {
+        return new Coordinates(this.x + other.x, this.y + other.y)
+    }
+
+    diff(other: Coordinates): Coordinates {
+        return new Coordinates(this.x - other.x, this.y - other.y)
+    }
+
+    reflection(): Coordinates {
+        return new Coordinates(-this.x, -this.y)
+    }
+
+    clamp(min: number, max: number): Coordinates {
+        const x = (this.x < min) ? min : (this.x > max) ? max : this.x
+        const y = (this.y < min) ? min : (this.y > max) ? max : this.y
+        return new Coordinates(x, y)
+    }
 }
+
 
 export class Environment {
     // This assumes a square environment.
@@ -23,30 +44,56 @@ export class Environment {
     entities: Entity[];
     entityRegistry: Map<number, Entity>;
     renderEngine: RenderEngine;
+    resolution: number;
+    canvas: HTMLCanvasElement
+    ctx: CanvasRenderingContext2D
 
-    constructor(dims: number, entities: Entity[], canvasId: string) {
+    constructor(dims: number, entities: Entity[], canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, resolution: number = 20) {
         this.dims = dims
         // Default environment allows walking anywhere.
         this.map = matrix.fill(dims, WALKABLE)
         this.entities = entities
         this.entityRegistry = new Map();
-        this.renderEngine = new RenderEngine(canvasId, dims);
+        this.resolution = resolution
+        this.canvas = canvas
+        this.ctx = ctx
+        this.renderEngine = new RenderEngine(this.canvas, this.ctx, dims, this.resolution);
+
         window.addEventListener("actorMove", (e: Event) => {
-            const evt = e as CustomEvent<ActorMove>;
+            const evt = e as CustomEvent<ActorEvent>;
             this.motionHandler(evt.detail)
+        })
+
+        window.addEventListener("particleEffects", (e: Event) => {
+            const evt = e as CustomEvent<Particle>;
+            this.entities.push(evt.detail);
+            this.entityRegistry.set(evt.detail.id, evt.detail)
+            this.particleHandler(evt.detail, this.entities.length - 1)
         })
     }
 
-    mapEntities() {
-        for (const entity of this.entities) {
-            const { coordinates, size } = entity;
+    mapEntities(tick: number = 0) {
+        type EntityMap = {
+            entity: Entity,
+            idx: number
+        }
+        const entitymap: Map<string, EntityMap> = new Map();
+        this.entities.forEach((entity, idx) => {
+            const { coordinates } = entity;
             const { x, y } = coordinates;
-            const { width, height } = size;
-            for (let row = y; row < y + height; row++) {
-                for (let col = x; col < x + width; col++) {
-                    this.map[row][col] = entity.id
-                    if (!this.entityRegistry.get(entity.id)) {
-                        this.entityRegistry.set(entity.id, entity)
+            entitymap.set(`${x},${y}`, { entity, idx });
+        })
+
+        for (let row = 0; row < this.dims; row++) {
+            for (let col = 0; col < this.dims; col++) {
+                const result = entitymap.get(`${col},${row}`);
+                if (result) {
+                    const { entity, idx } = result;
+                    this.map[row][col] = entity.id || WALKABLE;
+                    this.registerEntity(entity);
+                    if (entity && entity.type === EntityType.PARTICLE && tick === 1) {
+                        console.log(entity)
+                        this.particleHandler(entity as Particle, idx)
                     }
                 }
             }
@@ -54,12 +101,18 @@ export class Environment {
         return this;
     }
 
+    registerEntity(entity: Entity) {
+        if (!this.entityRegistry.get(entity.id)) {
+            this.entityRegistry.set(entity.id, entity)
+        }
+    }
+
     render(debug: boolean = false) {
         this.renderEngine.render(this.map, this.entities, debug)
     }
 
-    outOfBounds(actor: Actor, coordinates: Coordinates) {
-        const { width, height } = actor.size;
+    outOfBounds(entity: Entity, coordinates: Coordinates) {
+        const { width, height } = entity.size;
         const leakLeft = coordinates.x < 0
         const leakRight = coordinates.x + width > this.dims
         const leakTop = coordinates.y < 0
@@ -81,23 +134,23 @@ export class Environment {
         return null
     }
 
-    unregisterActorPos(actor: Actor) {
-        const { x, y } = actor.coordinates;
+    unregisterPos(e: Entity) {
+        const { x, y } = e.coordinates;
         this.map[y][x] = WALKABLE
     }
 
-    registerActorPos(actor: Actor) {
-        const { x, y } = actor.coordinates;
-        this.map[y][x] = actor.id
+    registerPos(e: Entity) {
+        const { x, y } = e.coordinates;
+        this.map[y][x] = e.id
     }
 
-    approveMotion(actor: Actor, nextPos: Coordinates) {
-        this.unregisterActorPos(actor);
+    allowMotion(actor: Actor, nextPos: Coordinates) {
+        this.unregisterPos(actor);
         actor.motionUpdate(nextPos);
-        this.registerActorPos(actor);
+        this.registerPos(actor);
     }
 
-    motionHandler(e: ActorMove) {
+    motionHandler(e: ActorEvent) {
         const { coordinates, actor } = e;
         let nextPos = coordinates;
         if (this.outOfBounds(actor, nextPos)) {
@@ -107,19 +160,46 @@ export class Environment {
         if (collidingEntity) {
             nextPos = this.collision(actor, collidingEntity, nextPos);
         }
-        this.approveMotion(actor, nextPos);
+        this.allowMotion(actor, nextPos);
     }
 
-    collision(entity: Entity, collidingEntity: Entity, nextPos: Coordinates): Coordinates {
-        switch (collidingEntity.onCollision) {
+    particleHandler(particle: Particle, index: number) {
+        const { coordinates, destination, speed } = particle;
+        if (this.outOfBounds(particle, destination)) {
+            this.entities.splice(index, 1);
+            this.unregisterPos(particle);
+            return;
+        }
+        const diff = new Coordinates(destination.x - coordinates.x, destination.y - coordinates.y)
+        const diffClamped = diff.clamp(-1, 1)
+        const nextPos = coordinates.add(new Coordinates(speed.x * diffClamped.x, speed.y * diffClamped.y));
+        console.log(coordinates)
+        console.log(nextPos)
+        const collidingEntity = this.collisionDetection(nextPos);
+        particle.coordinates = nextPos;
+
+        console.log(this.map)
+        console.log(this.entities[index])
+
+        if (collidingEntity) {
+            this.entities.splice(index, 1);
+            this.unregisterPos(particle);
+            return;
+        }
+    }
+
+    collision(actor: Actor, hitEntity: Entity, nextPos: Coordinates): Coordinates {
+        switch (hitEntity.onCollision) {
             case Collision.Block:
-                return entity.coordinates
+                return actor.coordinates
             case Collision.Bounce:
                 // Identify motion vector and reverse it.
-                return new Coordinates(entity.coordinates.x + 2, entity.coordinates.y + 2)
+                const diff = new Coordinates(nextPos.x - actor.coordinates.x, nextPos.y - actor.coordinates.y)
+                const reflection = diff.reflection()
+                return actor.coordinates.add(reflection)
             case Collision.Damage:
                 // Bounce and apply damage.
-                return entity.coordinates
+                return actor.coordinates
             case Collision.Equip:
                 // colliding entity should be removed from the map and entity registry.
                 return nextPos
@@ -128,20 +208,43 @@ export class Environment {
                 return nextPos
             case Collision.Explode:
                 // colliding entity should be removed from the map and entity registry.
-                return entity.coordinates
+                return actor.coordinates
             case Collision.Slow:
-                return entity.coordinates
+                return actor.coordinates
             default:
-                return entity.coordinates
+                return actor.coordinates
         }
     }
 
     run(grid: boolean = false) {
-        const loop = () => {
+        let lastTime = 0;
+        let frameCount = 0;
+        let fps = 0;
+        const loop = (currentTime: number) => {
+            const deltaTime = (currentTime - lastTime) / 1000; // Convert to seconds
+
+            // Calculate FPS
+            frameCount++;
+            if (deltaTime > 1) {
+                fps = frameCount / deltaTime;
+                frameCount = 0;
+                lastTime = currentTime;
+            }
+
+            if (fps > 60) {
+                this.mapEntities(1);
+            } else {
+                this.mapEntities(0);
+            }
             this.render(grid);
+
+            // Log FPS
+            console.log(`FPS: ${fps.toFixed(2)}`);
+
+            // Request the next frame
             window.requestAnimationFrame(loop);
-        }
-        loop();
+        };
+        loop(0);
     }
 }
 

76fca5a5dcd25ae146f1405afd905228682cbf06
ltbringer <amresh.venugopal@gmail.com>
2023-10-31 03:10:33 +0000

    add: entity type support for groupids, collisiondamage.

--- src/engine/entity/index.ts
+++ src/engine/entity/index.ts
@@ -18,30 +18,46 @@ export enum Collision {
     Slow,
 }
 
+export enum EntityType {
+    BASE,
+    ACTOR,
+    ITEM,
+    PARTICLE
+}
+
 export interface EntityConfig {
+    groupIds?: number[],
+    type?: EntityType,
     coordinates: Coordinates,
     size: Size,
-    color: string | null,
-    img: string | null,
+    color?: string | null,
+    img?: string | null,
     rigidity?: number,
     onCollision: Collision,
+    collisionDamage?: number,
 }
 
 export class Entity {
     id: number;
+    type?: EntityType;
+    groupIds: number[] = [];
     coordinates: Coordinates;
-    color: string | null;
-    img: string | null;
+    color?: string | null;
+    img?: string | null;
     size: Size;
     rigidity: number = 1;
     onCollision: Collision;
+    collisionDamage: number = 0;
 
     constructor(id: number, config: EntityConfig) {
         this.id = id
         this.coordinates = config.coordinates
         this.color = config.color || null
+        this.type = config.type || EntityType.BASE
         this.img = config.img || null
         this.onCollision = config.onCollision
+        this.collisionDamage = config.collisionDamage || 0
+        this.groupIds = config.groupIds || []
         if (config.color === null && config.img === null) {
             throw new Error('Entity must have a color or an image')
         }

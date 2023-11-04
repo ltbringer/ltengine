c562c5af61807fd0aaf24963085400d90bdd60d6
ltbringer <amresh.venugopal@gmail.com>
2023-11-04 22:44:19 +0000

    update: add and remove entities to grid.

--- src/engine/environment/grid.ts
+++ src/engine/environment/grid.ts
@@ -1,5 +1,5 @@
 import { Matrix } from '../../utils/matrix'
-import { Position } from '../../base/position'
+import { Position, IPosition } from '../../base/position'
 import { IEntity } from '../../base/entity'
 
 export const EMPTY = 0
@@ -14,16 +14,23 @@ export class Grid {
     this.state = new Matrix(rows, cols, EMPTY)
   }
 
-  insert(pos: Position, entity: IEntity) {
+  insert(pos: IPosition, entity: IEntity) {
     this.state.merge(entity.shape.asMatrix(), pos)
   }
 
+  remove(pos: IPosition, entity: IEntity) {
+    const shapeMatrix = entity.shape.asMatrix().fill(EMPTY)
+    this.state.merge(shapeMatrix, pos)
+  }
+
+  renderBlock(ctx: CanvasRenderingContext2D, pos: IPosition, scale: number) {
+    ctx.strokeStyle = 'black'
+    ctx.strokeRect(pos.y * scale, pos.x * scale, scale, scale)
+  }
+
   render(ctx: CanvasRenderingContext2D, scale: number) {
-    this.state.data.forEach((row, i) => {
-      row.forEach((col, j) => {
-        ctx.strokeStyle = 'black'
-        ctx.strokeRect(j * scale, i * scale, scale, scale)
-      })
+    this.state.data.forEach((row, x) => {
+      row.forEach((_, y) => this.renderBlock.call(this, ctx, { x, y }, scale))
     })
   }
 }

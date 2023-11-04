df16e219464fd05d8d075a854085b72bfb384937
ltbringer <amresh.venugopal@gmail.com>
2023-11-04 01:50:23 +0000

    refactor: remove old logic.

---
+++ src/engine/environment/grid.ts
@@ -0,0 +1,15 @@
+import { Position } from "../../engine/position";
+import { Shape } from "../../engine/entity/shape";
+
+class Grid2D {
+    rows: number;
+    cols: number;
+    constructor(rows: number, cols: number) {
+        this.rows = rows;
+        this.cols = cols;
+    }
+
+    insert(pos: Position, shape) {
+
+    }
+}

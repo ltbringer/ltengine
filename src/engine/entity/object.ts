746644da0532b2b0b901949a80d20d106a897466
ltbringer <amresh.venugopal@gmail.com>
2023-11-04 22:45:26 +0000

    update: render method for objects. refactor: unwanted properties.

--- src/engine/entity/object.ts
+++ src/engine/entity/object.ts
@@ -6,34 +6,28 @@ import { InvalidEntitySpecError } from './error'
 
 export class Obj implements IEntity {
   id: number
-  groupIds: string[]
+  groupIds: number[]
   type: string
   width: number
   height: number
-  symbol: string
   shape: Rectangle
   position: Position
   collisionEffect: CollisionEffects
   env: Environment
-  render: (ctx: CanvasRenderingContext2D) => void
-  onCollision: (entity: IEntity) => void
   constructor(config: IEntity, env: Environment) {
     // Representational properties
     this.id = config.id
     this.groupIds = config.groupIds
     this.type = 'obj'
-    this.symbol = config.symbol
 
     // Spatial properties
-    this.position = config.position
+    this.position = new Position(config.position.x, config.position.y);
     this.width = config.width
     this.height = config.height
     this.shape = new Rectangle(this.width, this.height, this.id)
     this.collisionEffect = config.collisionEffect
 
     this.env = env
-    this.render = config.render
-    this.onCollision = config.onCollision
     this.validate()
   }
 
@@ -44,8 +38,10 @@ export class Obj implements IEntity {
     if (this.height <= 0) {
       throw new InvalidEntitySpecError(this.type, '.height must be greater than 0')
     }
-    if (this.symbol.length !== 1) {
-      throw new InvalidEntitySpecError(this.type, '.symbol must be a single character')
-    }
+  }
+
+  render(ctx: CanvasRenderingContext2D, scale: number) {
+    ctx.fillStyle = 'black'
+    ctx.fillRect(this.position.x * scale, this.position.y * scale, this.width * scale, this.height * scale)
   }
 }

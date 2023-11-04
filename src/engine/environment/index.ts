95eb27173e2e54569b76a2230d7e02358bf4db3b
ltbringer <amresh.venugopal@gmail.com>
2023-11-04 17:11:31 +0000

    update: render environment method.

--- src/engine/environment/index.ts
+++ src/engine/environment/index.ts
@@ -1,12 +1,32 @@
-import { Grid } from './grid';
+import { Grid } from './grid'
 
-export class Environment2D {
-    m: number;
-    n: number;
-    grid: Grid;
-    constructor(m: number, n: number) {
-        this.m = m;
-        this.n = n;
-        this.grid = new Grid(m, n);
-    }
+export class Environment {
+  m: number
+  n: number
+  grid: Grid
+  canvas: HTMLCanvasElement
+  ctx: CanvasRenderingContext2D
+  pxToCell: number
+
+  constructor(
+    m: number,
+    n: number,
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D,
+    pxToCell: number
+  ) {
+    this.m = m
+    this.n = n
+    this.grid = new Grid(m, n)
+    this.canvas = canvas
+    this.ctx = ctx
+    this.pxToCell = pxToCell
+  }
+
+  render() {
+    this.ctx.fillStyle = 'black'
+    const width = this.m * this.pxToCell
+    const height = this.n * this.pxToCell
+    this.ctx.fillRect(0, 0, width, height)
+  }
 }

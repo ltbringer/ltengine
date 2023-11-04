f1d63bf17cbe5d802dd92b6db556b9dec58ff37b
ltbringer <amresh.venugopal@gmail.com>
2023-11-04 17:53:48 +0000

    add: key events and rendering logic for env.

--- src/engine/environment/index.ts
+++ src/engine/environment/index.ts
@@ -1,32 +1,69 @@
 import { Grid } from './grid'
 
+enum Mode {
+  GAME = 'game',
+  EDIT = 'edit'
+}
+
 export class Environment {
   m: number
   n: number
   grid: Grid
   canvas: HTMLCanvasElement
   ctx: CanvasRenderingContext2D
-  pxToCell: number
+  scale: number
+  mode: Mode
 
   constructor(
     m: number,
     n: number,
     canvas: HTMLCanvasElement,
     ctx: CanvasRenderingContext2D,
-    pxToCell: number
+    scale: number
   ) {
     this.m = m
     this.n = n
     this.grid = new Grid(m, n)
     this.canvas = canvas
     this.ctx = ctx
-    this.pxToCell = pxToCell
+    this.scale = scale
+    this.mode = Mode.GAME
+    this.canvas.addEventListener('keydown', this.handleKeyDown.bind(this))
+  }
+
+  handleKeyDown(e: KeyboardEvent) {
+    switch (e.code) {
+      case 'KeyE':
+        this.mode = Mode.EDIT
+        break;
+      case 'KeyG':
+        this.mode = Mode.GAME
+        break;
+      default:
+        break;
+    }
   }
 
   render() {
-    this.ctx.fillStyle = 'black'
-    const width = this.m * this.pxToCell
-    const height = this.n * this.pxToCell
-    this.ctx.fillRect(0, 0, width, height)
+    let times: number[] = [];
+    let fpsHist: number[] = [];
+    let avgFPS = 0;
+    let fps = 0;
+    const renderLoop = () => {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+      const now = performance.now()
+      times = times.filter((timestamp) => timestamp >= now - 1000);
+      times.push(now);
+      fps = times.length;
+      fpsHist.push(fps);
+      if (fpsHist.length > 100) {
+        avgFPS = fpsHist.reduce((a, b) => a + b, 0) / fpsHist.length;
+        fpsHist = [];
+      }
+      this.ctx.fillText(`FPS: ${avgFPS.toFixed(1)}`, this.canvas.width - 100, 20)
+      this.grid.render(this.ctx, this.scale)
+      requestAnimationFrame(renderLoop)
+    }
+    renderLoop()
   }
 }

87f69dcf1a7d546997e0850face4cce71379eb39
ltbringer <amresh.venugopal@gmail.com>
2023-11-04 17:10:43 +0000

    update: Shape. We need only rectangles.

--- src/base/shape.ts
+++ src/base/shape.ts
@@ -1,18 +1,22 @@
-import { Matrix } from "../utils/matrix";
+import { Matrix } from '../utils/matrix'
 
-export interface Shape {
-    asMatrix(value: number): Matrix;
+export interface IRectangle {
+  width: number
+  height: number
+  value: number
 }
 
-export class Rectangle {
-    width: number;
-    height: number;
-    constructor(width: number, height: number) {
-        this.width = width;
-        this.height = height;
-    }
+export class Rectangle implements IRectangle {
+  width: number
+  height: number
+  value: number
+  constructor(width: number, height: number, value: number) {
+    this.width = width
+    this.height = height
+    this.value = value
+  }
 
-    asMatrix(value: number): Matrix {
-        return new Matrix(this.height, this.width, value);
-    }
+  asMatrix(): Matrix {
+    return new Matrix(this.height, this.width, this.value)
+  }
 }

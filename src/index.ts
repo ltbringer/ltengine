3e7c3d06e881624ead8b1f25f6eed5c8e86f8e4b
ltbringer <amresh.venugopal@gmail.com>
2023-11-04 23:31:49 +0000

    add: actor to ui test.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,38 @@
 import { Environment } from "./engine/environment";
+import { Actor } from "./engine/entity/actor";
+import { CollisionEffects } from "./base/entity";
 
-const canvas = document.createElement('canvas');
-if (!canvas) {
-    throw new Error('Could not find canvas element');
+const viewSetup = () => {
+    const canvas = document.createElement('canvas');
+    if (!canvas) {
+        throw new Error('Could not find canvas element');
+    }
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
+    document.body.appendChild(canvas);
+    const ctx = canvas.getContext('2d');
+    if (!ctx) {
+        throw new Error('Could not create canvas context');
+    }
+    return { canvas, ctx };
 }
-canvas.width = document.body.clientWidth;
-canvas.height = document.body.clientHeight;
-document.body.appendChild(canvas);
-const ctx = canvas.getContext('2d');
-if (!ctx) {
-    throw new Error('Could not create canvas context');
-}
+
+
+const { canvas, ctx } = viewSetup();
 const env = new Environment(50, 50, canvas, ctx, 20);
+env.load();
+
+const p1Id = env.getId();
+const p1Pos = env.grid.spawnPoint();
+const p1 = new Actor({
+    id: p1Id,
+    groupIds: [],
+    position: p1Pos,
+    width: 1,
+    height: 1,
+    color: 'red',
+    collisionEffect: CollisionEffects.BLOCK,
+}, env);
+
 env.render();
+env.registerEntity(p1);
